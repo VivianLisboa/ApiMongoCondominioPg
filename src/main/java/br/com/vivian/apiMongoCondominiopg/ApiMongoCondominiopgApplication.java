d918f428be51d800037f327c2ad00a88562d5372
VivianLisboa <ribeiro.vivian@gmail.com>
2019-09-23 21:40:59 -0300

    primeiro commit

---
+++ src/main/java/br/com/vivian/apiMongoCondominiopg/ApiMongoCondominiopgApplication.java
@@ -0,0 +1,13 @@
+package br.com.vivian.apiMongoCondominiopg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMongoCondominiopgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMongoCondominiopgApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/vivian/apiMongoCondominiopg/controller/CondominioController.java
@@ -0,0 +1,23 @@
+package br.com.vivian.apiMongoCondominiopg.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/condominiopg")
+public class CondominioController {
+	
+//	public CondominioService condominioService;
+//	
+//	@Autowired
+//	public CondominioController(CondominioService condominioService) {
+//		this.condominioService = condominioService;
+//	}
+//	
+//	@GetMapping(value ="/condominios")
+//	public List<Condominios> getCondominio
+	
+
+}
